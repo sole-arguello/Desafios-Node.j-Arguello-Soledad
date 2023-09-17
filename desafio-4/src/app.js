c7e7c7ebf23a4d29ddfbb92387c2fc389d37599a
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-09-17 13:12:40 -0300

    traigo los productos desde el service

--- desafio-4/src/app.js
+++ desafio-4/src/app.js
@@ -5,10 +5,13 @@ import { Server } from 'socket.io';
 //importo dirname para obtener la ruta de mis archivos y path para unir diferentes rutas
 import { __dirname } from './utils.js';
 import path from 'path';
+//importo los productos
+import { ProductManager } from './persistence/productManager.js'; 
 //importo rutas propias y las de handlebars
 import { productsRoutes } from './routes/products.routes.js';
 import { cartsRouter } from './routes/carts.routes.js';
 import { viewsRouter } from './routes/views.routes.js';
+import { productsService } from './persistence/index.js';
 
 const port = 8080;
 const app = express();
@@ -20,7 +23,13 @@ app.use(express.static(path.join(__dirname, '/public')));
 //configuro para websocket del lado del server
 const httpSever = app.listen(port, () => {console.log(`app listening at http://localhost:${port}`);})
 const io = new Server(httpSever);
-io.on('connection', (socket) => {console.log('Cliente Conectado');})
+io.on('connection', async (socket) => {
+    console.log('Cliente Conectado');
+    //obtengo todos los productos
+    const products = await productsService.getProducts();
+    //envio los productos al cliente
+    socket.emit('productsArray', products);
+})
 
 //configuracion de handlebars, motor de plantillas
 app.engine('.hbs', engine({extname: '.hbs'}));
--- desafio-4/src/public/js/realTime.js
+++ desafio-4/src/public/js/realTime.js
@@ -1,1 +1,7 @@
 const socketClient = io()
+
+
+//recibo los productos del lado del server
+socketClient.on('productsArray', (dataProducts) => {
+     console.log(dataProducts);
+})
