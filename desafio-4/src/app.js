58683414e003fe9def15bc76e2dec2e3558e53e4
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-09-17 12:00:22 -0300

    creo y configuro la carpeta public

--- desafio-4/src/app.js
+++ desafio-4/src/app.js
@@ -1,31 +1,35 @@
 import express from 'express';
-//importo libreria handlebars
+//importo libreria handlebars y socket.io
 import { engine } from 'express-handlebars';
+import { Server } from 'socket.io';
 //importo dirname para obtener la ruta de mis archivos y path para unir diferentes rutas
 import { __dirname } from './utils.js';
 import path from 'path';
-
-import { productsRoutes } from './routes/products.routes.js';
-import { cartsRouter } from './routes/carts.routes.js';
+//importo rutas propias y las de handlebars
+// import { productsRoutes } from './routes/products.routes.js';
+// import { cartsRouter } from './routes/carts.routes.js';
+import { viewsRouter } from './routes/views.routes.js';
 
 const port = 8080;
 const app = express();
 app.use(express.json());
 
+//midleware
+app.use(express.static(path.join(__dirname, '/public')));
 
-app.listen(port, () => {console.log(`app listening at http://localhost:${port}`);})
-//rutas
-app.use("/api/products", productsRoutes);
-app.use("/api/carts", cartsRouter)
+//configuro para websocket del lado del server
+const httpSever = app.listen(port, () => {console.log(`app listening at http://localhost:${port}`);})
+const io = new Server(httpSever);
 
 //configuracion de handlebars, motor de plantillas
 app.engine('.hbs', engine({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, '/views'));//src/views
 
+//rutas
+app.use(viewsRouter)
+// app.use("/api/products", productsRoutes);
+// app.use("/api/carts", cartsRouter)
 
 
-app.get('/', (req, res) => {
-    res.render('form')
-})
 
