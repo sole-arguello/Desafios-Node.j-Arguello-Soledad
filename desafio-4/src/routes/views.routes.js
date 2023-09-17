72843b5adcc21757900d101f35d219c9f3aae479
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-09-17 11:19:31 -0300

    creo la ruta de home

---
+++ desafio-4/src/routes/views.routes.js
@@ -0,0 +1,32 @@
+import { Router } from 'express';
+import { productsService } from '../persistence/index.js';
+const router = Router();
+
+//ruta para la vista home de todos los productos
+router.get('/', async (req, res) => {
+    try {
+        const products = await productsService.getProducts();
+        const existProducts = products.length > 0;
+        if (existProducts) {
+            res.render('home', { products : products });//podria ir solo products
+        }else{
+            res.render('No hay productos para mostrar');
+        }
+        
+    } catch (error) {
+       res.status(500).json({ message: error.message }); 
+    }
+   
+})
+
+//ruta para productos en tiempo real Eliminar y Crear
+router.get('/realTimeProducts', (req, res) => {
+    try {
+        res.render('realTime');
+    } catch (error) {
+        res.status(500).json({ message: error.message });        
+    }
+})
+
+
+export { router as viewsRouter }
