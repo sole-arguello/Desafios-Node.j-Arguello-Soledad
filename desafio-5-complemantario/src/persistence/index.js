e11d17ce69bba03234b1658a70cbfa3a4006fba2
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-10-02 00:30:50 -0300

    configuro el servicio

--- desafio-5-complemantario/src/persistence/index.js
+++ desafio-5-complemantario/src/persistence/index.js
@@ -3,7 +3,14 @@ import { CartsManager } from './files/cartsManager.js';
 import { __dirname } from "../utils.js";
 import path from "path";
 
-console.log('dirname', path.join(__dirname, '/files'));
+import { ProductsManagerMongo } from "./dao/mongo/productsManagerMongo.js";
+import { CartsManagerMongo } from "./dao/mongo/cartsManagerMongo.js";
+
+
+export const productsServiceMongo = new ProductsManagerMongo()
+export const cartsServiceMongo = new CartsManagerMongo();
+
+//console.log('dirname', path.join(__dirname, '/files'));
 
 export const productsService = new ProductManager(path.join(__dirname, '/data/products.json'));
 export const cartsService = new CartsManager(path.join(__dirname, '/data/carts.json'));
