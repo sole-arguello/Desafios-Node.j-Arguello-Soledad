2b4fa341adb9594e51fdde6aaabad7ec884b157c
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-10-02 15:33:10 -0300

    creo los manager de mongo para carts, products, message

---
+++ desafio-5-complemantario/src/dao/mongo/cartsManagerMongo.js
@@ -0,0 +1,18 @@
+export class CartsManagerMongo {
+    
+    constructor(){}
+
+    async getCarts() {
+        
+    }
+    async getCartsById(id){
+        
+    }
+    async createCart(){
+        
+    }
+
+    async addProduct(){
+        
+    }
+}
