a5d7020dcd972b86aab0aa4a0382f9e82b6f9996
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-10-05 13:11:30 -0300

    limpio codigo

--- desafio-5-complemantario/src/dao/index.js
+++ desafio-5-complemantario/src/dao/index.js
@@ -1,7 +1,5 @@
-// import { ProductManager } from "./files/productManager.js";
-// import { CartsManager } from './files/cartsManager.js';
+
 import { __dirname } from "../utils.js";
-//import path from "path";
 
 
 import { ProductsManagerMongo } from './mongo/productsManagerMongo.js';
@@ -12,7 +10,3 @@ export const productsService = new ProductsManagerMongo();
 export const cartsService = new CartsManagerMongo();
 export const chatService = new ChatManagerMongo();
 
-//console.log('dirname', path.join(__dirname, '/files'));
-
-// export const productsService = new ProductManager(path.join(__dirname, '/data/products.json'));
-// export const cartsService = new CartsManager(path.join(__dirname, '/data/carts.json'));
