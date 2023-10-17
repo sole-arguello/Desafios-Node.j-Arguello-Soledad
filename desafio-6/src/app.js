b54d46fb29ddf3c28b8803aca2f7d14a863e4469
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-10-17 17:13:26 -0300

    configuro session y connect-mongo

--- desafio-6/src/app.js
+++ desafio-6/src/app.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import session from 'express-session';
+import MongoStore from 'connect-mongo';
 import { __dirname } from './utils.js';//importo dirname para obtener la ruta de mis archivos y path para unir diferentes rutas
 import path from 'path';
 import { engine } from 'express-handlebars'; //importo libreria handlebars y socket.io
 import { Server } from 'socket.io';
 import{ connectDB } from './config/dbConnection.js';//importo connectDB
+
+
+
 import { chatService } from './dao/index.js'; //importo el servicio de caht para uasrlo en socket
 import { productsService } from './dao/index.js';//importo el servicio de caht para uasrlo en socket
 
@@ -12,6 +17,7 @@ import { productsService } from './dao/index.js';//importo el servicio de caht p
 import { viewsRouter } from './routes/views.routes.js';
 import { productsRouter } from './routes/products.routes.js';
 import { cartsRouter } from './routes/carts.routes.js';
+import { usersSessionsRouter } from './routes/usersSessions.routes.js';
 
 const port = 8080;//configuro puerto
 const app = express();
@@ -100,9 +106,20 @@ app.engine('.hbs', engine({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, '/views'));//src/views
 
+//configuracion de session
+app.use(session ({
+    store: MongoStore.create({
+        ttl: 60,
+        mongoUrl: 'mongodb+srv://soledadar:g04D4zMd9O4y2GvK@cluster0.njbseut.mongodb.net/tiendaDB?retryWrites=true&w=majority'
+    }),
+    secret: 'secretSessions',
+    resave: true,
+    saveUninitialized: true
+}))
 
 
 //rutas trabajadas con mongo
-app.use(viewsRouter); 
+app.use(viewsRouter);
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartsRouter)
+app.use('api/sessions', usersSessionsRouter);
