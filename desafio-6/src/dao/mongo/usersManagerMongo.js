27b7071df25a9a84c580c6366e9072cd702ca65d
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-10-17 17:44:56 -0300

    creo el manager que trae el usuario registrado

--- desafio-6/src/dao/mongo/usersManagerMongo.js
+++ desafio-6/src/dao/mongo/usersManagerMongo.js
@@ -16,6 +16,15 @@ export class UsersManagerMongo {
         }
         
     }
+    async getUser(email){
+        try {
+            const user = await this.model.findOne({email})
+            return user
+        } catch (error) {
+            console.log('get user', error.message);
+            throw new Error('El Usuario no se encuentra registrado', error.message);
+        }
+    }
 
 
 }
