72702624266ebfa755a089e97a34e690a376c227
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-10-20 09:35:55 -0300

    creo la funcion que hashea las password y la que las compara

--- desafio-6/src/utils.js
+++ desafio-6/src/utils.js
@@ -3,5 +3,14 @@ import { fileURLToPath } from "url";
 
 export const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-//dentro del proyecto ejecuto node src/utils.js
-//console.log('__dirname',__dirname);
+export const createHash = (password) => {//recibo la contraseña del form
+    const salt = bcrypt.genSaltSync(10);//crea el algoritmo
+    const hash = bcrypt.hashSync(password, salt);//recibe la contraseña y el algoritmo
+    return hash
+    
+}
+
+//funcion para comparar los hash
+export const inValidPassword = (password, user) => {//recibe la contraseña nueva y los datos del usuario
+    return bcrypt.compareSync(password, user.password)
+}
