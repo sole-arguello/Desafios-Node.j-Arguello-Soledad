78fa21d1918881b1ff042495502aaa85cdbda4ab
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-10-20 12:39:08 -0300

    haciendo debag funcion isInvalidPassword

--- desafio-6/src/routes/usersSessions.routes.js
+++ desafio-6/src/routes/usersSessions.routes.js
@@ -8,12 +8,9 @@ const router = Router();
 router.post('/register', async (req, res) => {
     try {
         const newUser = req.body;
-        console.log('nuevo usuario', newUser)
         //aplico hash a la contraseña
         newUser.password = createHash(newUser.password);
-        console.log('contraseña encriptada', newUser)
         const userCreated = await usersService.createUsers(newUser);
-        console.log('Usuario creado', userCreated)
         res.render('login', {message: 'Usuario registrado con exito'});
     } catch (error) {
         res.status(500).render('register', {error: 'No se pudo registrar el usuario'});
@@ -23,20 +20,26 @@ router.post('/register', async (req, res) => {
 //logueo al usuario se admin o usuario
 router.post('/login', async (req, res) => {
     try {
-
+        //recibo el cuerpo del form 
         const loginUser = req.body;
+        const passwordLogin = loginUser.password
+        console.log('contraseña hash', passwordLogin)
         if(loginUser.email === 'admin@coder.com' && loginUser.password === 'admin') {
             req.session.email = loginUser.email
             req.session.role = 'admin'
         }else{
             const user = await usersService.getUser(loginUser.email);
+            
+            console.log( 'usuario encontrado', user)
             //verifico si el usuario existe
             if(!user) {
                 return res.render('login', {error: 'Usuario no registrado'});
             }
            //remplazo validacion de contraseña
-            if(isValidPassword(loginUser.password, user)) {
-                return res.render('login', {errorLogin: 'Credenciles invalidas'});
+            if(isValidPassword(passwordLogin, user)) {
+                console.log( 'contrase login', passwordLogin, 'usuario', user)
+
+                return res.render('login', {error: 'Credenciles invalidas'});
             }
             //si todo es ok, creo la sesion del usuario 
             req.session.first_name = user.first_name;
@@ -46,6 +49,7 @@ router.post('/login', async (req, res) => {
             req.session.role = user.role;
             
         }
+
         res.redirect('/');//redirecciono a home y ya tiene acceso a navegar en la page     
     } catch (error) {
         res.status(500).render('login', {error: 'No se pudo iniciar sesion, para este usuario'});
