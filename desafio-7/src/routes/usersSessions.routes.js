dc9bd7705dd53241bcd324e563a4efd356bf7db0
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-10-20 22:06:36 -0300

    llevo la logica a passport

--- desafio-7/src/routes/usersSessions.routes.js
+++ desafio-7/src/routes/usersSessions.routes.js
@@ -1,57 +1,60 @@
 import { Router } from "express";
 import { usersService } from "../dao/index.js";
 import { createHash, isValidPassword } from "../utils.js";
+import passport from "passport";
 
 const router = Router();
 
 //registro al ususario
-router.post('/register', async (req, res) => {
-    try {
-        const newUser = req.body;
-        //aplico hash a la contraseña
-        newUser.password = createHash(newUser.password);
-        const userCreated = await usersService.createUsers(newUser);
+router.post('/register', passport.authenticate('registerLocalStrategy', {
+    failureRedirect: '/api/sessions/fail-register'}), async (req, res) => {
         res.render('login', {message: 'Usuario registrado con exito'});
-    } catch (error) {
-        res.status(500).render('register', {error: 'No se pudo registrar el usuario'});
-    }
+    
+})
+router.get('/fail-register', (req, res) => {
+    res.render('register', {error: 'No se pudo registrar el usuario'});
 })
 
 //logueo al usuario se admin o usuario
-router.post('/login', async (req, res) => {
-    try {
-        //recibo el cuerpo del form 
-        const loginUser = req.body;
-        const passwordLogin = loginUser.password
-        if(loginUser.email === 'admin@coder.com' && loginUser.password === 'admin') {
-            req.session.email = loginUser.email
-            req.session.role = 'admin'
-        }else{
-            const user = await usersService.getUser(loginUser.email);
-            //verifico si el usuario existe
-            if(!user) {
-                return res.render('login', {error: 'Usuario no registrado'});
-            }
-           //remplazo validacion de contraseña
-            if(!isValidPassword(passwordLogin, user)) {
+// router.post('/login',
+// passport.authenticate('loginLocalStrategy',
+// {failureRedirect: '/api/sessions/fail-login'}), async (req, res) => {
+//     try {
+//         //recibo el cuerpo del form
+//         const loginUser = req.body;
+//         const passwordLogin = loginUser.password
+//         if(loginUser.email === 'admin@coder.com' && loginUser.password === 'admin') {
+//             req.session.email = loginUser.email
+//             req.session.role = 'admin'
+//         }else{
+//             const user = await usersService.getUser(loginUser.email);
+//             //verifico si el usuario existe
+//             if(!user) {
+//                 return res.render('login', {error: 'Usuario no registrado'});
+//             }
+//            //remplazo validacion de contraseña
+//             if(!isValidPassword(passwordLogin, user)) {
 
-                return res.render('login', {error: 'Credenciles invalidas'});
-            }
-            //si todo es ok, creo la sesion del usuario 
-            req.session.first_name = user.first_name;
-            req.session.last_name = user.last_name;
-            req.session.age = user.age;
-            req.session.email = user.email;
-            req.session.role = user.role;
+//                 return res.render('login', {error: 'Credenciles invalidas'});
+//             }
+//             //si todo es ok, creo la sesion del usuario 
+//             req.session.first_name = user.first_name;
+//             req.session.last_name = user.last_name;
+//             req.session.age = user.age;
+//             req.session.email = user.email;
+//             req.session.role = user.role;
             
-        }
+//         }
 
-        res.redirect('/');//redirecciono a home y ya tiene acceso a navegar en la page     
-    } catch (error) {
-        res.status(500).render('login', {error: 'No se pudo iniciar sesion, para este usuario'});
-    }
+//         res.redirect('/');//redirecciono a home y ya tiene acceso a navegar en la page     
+//     } catch (error) {
+//         res.status(500).render('login', {error: 'No se pudo iniciar sesion, para este usuario'});
+//     }
 
-})
+// })
+// router.get('/fail-login', (req, res) => {
+    
+// })
 
 //para eliminar la seccion
 router.get('/logout', (req, res) => {
