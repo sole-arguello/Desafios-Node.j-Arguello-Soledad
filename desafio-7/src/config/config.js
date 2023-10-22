163b3d6d11e841a8cd68c2e690c483c0f1014470
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-10-21 23:49:20 -0300

    corrijo variable de entorno

--- desafio-7/src/config/config.js
+++ desafio-7/src/config/config.js
@@ -10,8 +10,9 @@ export const config = {
     mongo: {
         url: process.env.MONGO_URL
     },
+    
     github: {
-        callbackUrl: process.env.GITHUB_CALLBACK,
+        callbackUrl: process.env.GITHUB_CALLBACK_URL,
         clientId: process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }
