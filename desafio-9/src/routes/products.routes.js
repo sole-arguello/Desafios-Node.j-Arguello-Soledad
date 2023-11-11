b3f4ac1b6f053c2a2d03538f061da14d690d44b4
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-11-11 11:49:35 -0300

    remplazo por controller

--- desafio-9/src/routes/products.routes.js
+++ desafio-9/src/routes/products.routes.js
@@ -6,48 +6,13 @@ const router = Router();
 
 router.get('/', ProductsController.getProducts)
 
-router.get('/:id', async (req, res) => {
-    try {
-        const productId = req.params.id;
-        const resultado = await productsService.getProductById(productId);
-        res.json({ status: 'success', data: resultado });
-    }
-    catch (error) {
-        res.json({ status: 'error',  message: error.message });
-    }
-})
+router.get('/:id', ProductsController.getProductsId)
+
 //localhost:8080/api/products
-router.post('/', async (req, res) => {
-    try {
-        const productInfo = req.body;//cargo los datos en postman
-        const resultado = await productsService.createProduct(productInfo);
-        res.json({ status: 'success', data: resultado });
-    } catch (error) {
-        res.json({ status: 'error',  message: error.message });
-    }
-})
+router.post('/', ProductsController.createProduct)
 
-router.put('/:id', async (req, res) => {
-    try {
-        const id = req.params.id;
-        const product = req.body;
-        const resultado = await productsService.updateProduct(id, product);
-        res.json({ status: 'success', data: resultado });
-    }
-    catch (error) {
-        res.json({ status: 'error',  message: error.message });
-    }
-})
+router.put('/:id', ProductsController.updateProduct)
 
-router.delete('/:id', async (req, res) => {
-    try {
-        const id = req.params.id;
-        const resultado = await productsService.deleteProduct(id);
-        res.json({ status: 'success', data: resultado });
-    }
-    catch (error) {
-        res.json({ status: 'error',  message: error.message });
-    }
-})
+router.delete('/:id', ProductsController.deleteProduct)
 
 export { router as productsRouter }
