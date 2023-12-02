71dea923abcc4dcb6b9f93f12a450ef435a436a4
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-12-02 20:06:03 -0300

    aplico manejo de errores en login

--- desafio-10/src/controller/usersSessions.controller.js
+++ desafio-10/src/controller/usersSessions.controller.js
@@ -1,5 +1,7 @@
 import { generateToken } from "../utils.js";
-
+import { EError } from "../service/errors/enums.js";
+import { generateLoginErrorInfo } from '../service/errors/infoDictionary.js'
+import { CustomError } from "../service/errors/customErrors.js";
 export class UsersSessionsController {
     static renderRegister = async (req, res) => {
         res.render('login', {
@@ -20,18 +22,32 @@ export class UsersSessionsController {
         res.cookie('authLogin', token, {maxAge: 43200000, httpOnly: true});
         res.redirect('/profile')
     }
-    static renderLogin = async (req, res) => {
+    static renderLogin = async (req, res, next) => {
         try {
-            const user = req.user;
+            const {email, password} = req.body;
+
+            if(!email || !password){
+                const loginErrorInfo = generateLoginErrorInfo({email, password});
+                CustomError.createError({
+                    name: 'Error login ',
+                    cause: loginErrorInfo,
+                    message: 'Credenciales incorrectas',
+                    code: EError.INVALID_LOGIN,
+                })
+                
+            }
+            const user = req.user
             const token = generateToken(user);
             res.cookie('cookieLogin', token, {maxAge: 43200000, httpOnly: true});
             res.redirect('/');//redirecciono a home y ya tiene acceso a navegar en la page
-            console.log('Usuario Logueado', req.user.role);
+            console.log('Loguin', req.user.role);
         } catch (error) {
+            
             res.render('login', {
                 style: "login.css",
                 error: 'Credenciales incorrectas'
             });
+        
         }
     }
     static renderLoginFail = async (req, res) => {
