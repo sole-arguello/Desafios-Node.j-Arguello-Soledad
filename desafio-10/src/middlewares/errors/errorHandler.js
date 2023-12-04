c64969736dd277df6aef6e14ff17b6b4da96354f
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-12-04 16:45:59 -0300

    corrijo codigo

--- desafio-10/src/middlewares/errors/errorHandler.js
+++ desafio-10/src/middlewares/errors/errorHandler.js
@@ -1,25 +1,25 @@
 import {EError } from '../../service/errors/enums.js';
 
 export const errorHandler = (error, req, res, next) => {
-    console.log(error.cause)
+    console.log('paso por errorHandler',error.cause)
     switch (error.code) {
         case EError.INVALID_TYPES_ERROR:
 
-            res.json({ status: 'error', message: error.cause })
+            res.json({ status: 'error', error: error.message })
             break
 
         case EError.DATABASE_ERROR:
-            res.json({ status: 'error', message: error.cause })
+            res.json({ status: 'error', error: error.cause })
             break
 
         case EError.ROUTING_ERROR:
-            res.json({ status: 'error', message: error.cause })
+            res.json({ status: 'error', error: error.cause })
             break
 
         case EError.INVALID_LOGIN:
-            res.json({ status: 'error', message: error.cause })
+            res.json({ status: 'error', error: error.message })
             break;
         default:
-            res.json({ status: 'error', message:'Unhandled Error' })
+            res.json({ status: 'error', error:'Unhandled Error' })
     }
 }
