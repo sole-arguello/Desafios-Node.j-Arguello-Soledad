3e20ccb0f36a7ac838b1657631111d595f57b689
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-12-04 16:45:00 -0300

    agrego mensajes de consola

--- desafio-10/src/controller/usersSessions.controller.js
+++ desafio-10/src/controller/usersSessions.controller.js
@@ -1,20 +1,18 @@
 import { generateToken } from "../utils.js";
-import { EError } from "../service/errors/enums.js";
-import { generateLoginErrorInfo } from '../service/errors/infoDictionary.js'
-import { CustomError } from "../service/errors/customErrors.js";
+
 export class UsersSessionsController {
     static renderRegister = async (req, res) => {
         res.render('login', {
             style: "login.css",
             message: `Hola, ${req.user.first_name} te has registrado con exito`
-        });
+        }, console.log('paso por renderRegister'));
     
     }
     static renderRegisterFail = async (req, res) => {
         res.render('register', {
             style: "register.css",
             error: 'Error al registrar el usuario'
-        });
+        }, console.log('paso por renderRegisterFail'));
     }
     static renderRegisterGithub = async (req, res) => {
         const user = req.user;
@@ -22,35 +20,23 @@ export class UsersSessionsController {
         res.cookie('authLogin', token, {maxAge: 43200000, httpOnly: true});
         res.redirect('/profile')
     }
-    static renderLogin = async (req, res, next) => {
+    static renderLogin = async (req, res) => {
         try {
-            const {email, password} = req.body;
-
-            if(!email || !password){
-                const loginErrorInfo = generateLoginErrorInfo({email, password});
-                CustomError.createError({
-                    name: 'Error login ',
-                    cause: loginErrorInfo,
-                    message: 'Credenciales incorrectas',
-                    code: EError.INVALID_LOGIN,
-                })
-                
-            }
-            const user = req.user
+            //manejo de errores en middlewares
+            console.log('paso por renderLogin, loguin rol: ', req.user.role);
+            const user = req.user;
             const token = generateToken(user);
             res.cookie('cookieLogin', token, {maxAge: 43200000, httpOnly: true});
             res.redirect('/');//redirecciono a home y ya tiene acceso a navegar en la page
-            console.log('Loguin', req.user.role);
         } catch (error) {
-            
             res.render('login', {
                 style: "login.css",
                 error: 'Credenciales incorrectas'
             });
-        
         }
     }
     static renderLoginFail = async (req, res) => {
+        
         res.render('login', {
          style: "login.css",
          error: 'Para navegar debe iniciar sesion'
@@ -58,6 +44,7 @@ export class UsersSessionsController {
      }
      static renderLogout = async (req, res) => {
         try {
+            console.log('paso por renderLogout');
             res.clearCookie('cookieLogin');
             //una vez cerrada la sesion lo redirige a login
             res.redirect('/login');
