467e38770bc7145b4cf10b290bf8cf4a8bc19f64
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-12-05 21:34:03 -0300

    agrego autorizaciones

--- desafio-10/src/routes/views.routes.js
+++ desafio-10/src/routes/views.routes.js
@@ -5,28 +5,28 @@ import { ViewsController } from '../controller/views.controller.js';
 const router = Router();
 
 //ruta para la vista home de todos los productos
-router.get('/', jwtAuth, ViewsController.renderViewsHome)
+router.get('/', jwtAuth, authorization(['user', 'admin']), ViewsController.renderViewsHome)
 
 //ruta para login
-router.get('/login', ViewsController.renderViewsLogin)
+router.get('/login', authorization(['user', 'admin']), ViewsController.renderViewsLogin)
 
 //ruta para register
 router.get('/register', ViewsController.renderViewsRegister)
 
 //ruta para el perfil de usuario
-router.get('/profile', jwtAuth, ViewsController.renderViewsProfile)
+router.get('/profile', jwtAuth, authorization(['user, admin']), ViewsController.renderViewsProfile)
 
 //ruta para productos en tiempo real crear y Eliminar 
 router.get('/realTimeProducts', jwtAuth, authorization(['admin']),ViewsController.renderViewsRealTime)
 
 //message para linkear / caht es la renderizacion hacia el chat 
-router.get('/message', jwtAuth, authorization(['user']),ViewsController.renderViewsMessage)
+router.get('/message', jwtAuth, authorization(['user, admin']),ViewsController.renderViewsMessage)
 
 //pagiante// localhost:8080?page=1 ... 2 ...3 ..etc
-router.get('/products', jwtAuth, ViewsController.renderViewsProducts)
+router.get('/products', jwtAuth, authorization(['user', 'admin']), ViewsController.renderViewsProducts)
 
 //ruta hardcodeada localhost:8080/cart/6525e395443bd76c765dd0ee
-router.get('/cart/:cid', ViewsController.renderViewsCart)
+router.get('/cart/:cid', authorization(['user', 'admin']), ViewsController.renderViewsCart)
 
 // viewsRouter.get("/cart", async (req, res) => {
 //     const cartId = await cartsModel.findOne().sort({ carts: -1 });
@@ -35,7 +35,7 @@ router.get('/cart/:cid', ViewsController.renderViewsCart)
 //     res.render("cart", { products: productsCart})
 // });
 //http://localhost:8080/mokingProducts
-router.get('/mokingProducts', ViewsController.mockingProducts)
+router.get('/mokingProducts', authorization(['user', 'admin']), ViewsController.mockingProducts)
 
 export { router as viewsRouter }
 
