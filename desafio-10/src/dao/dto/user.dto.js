196da75ba30c38119113cdb3763059a8b32a9225
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-12-06 10:34:09 -0300

    corrijo codigo

--- desafio-10/src/dao/dto/user.dto.js
+++ desafio-10/src/dao/dto/user.dto.js
@@ -1,8 +1,6 @@
 export class UsersDto {
     constructor(userInfo) {
         this.full_name = `${userInfo.first_name} ${userInfo.last_name}`.toUpperCase();
-        this.first_name = userInfo.first_name,
-        this.last_name = userInfo.last_name,
         this.email = userInfo.email,
         this.age = userInfo.age,
         this.role = userInfo.role,
