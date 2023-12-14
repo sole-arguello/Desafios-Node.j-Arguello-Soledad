28b9e397622cca406a89a96ca00c7fc592931e00
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-12-14 00:36:28 -0300

    elimino archivo

--- desafio-11-bis/src/dao/dto/user.dto.js
+++
@@ -1,9 +0,0 @@
-export class UsersDto {
-    constructor(userInfo) {
-        this.full_name = `${userInfo.first_name} ${userInfo.last_name}`.toUpperCase();
-        this.email = userInfo.email,
-        this.age = userInfo.age,
-        this.role = userInfo.role,
-        this.cart = userInfo.cart;
-    }
-  }
