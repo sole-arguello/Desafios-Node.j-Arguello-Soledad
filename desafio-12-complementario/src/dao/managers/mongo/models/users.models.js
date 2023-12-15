1005c5c2364d5024b57df3beae111bed661e29a6
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-12-14 23:24:54 -0300

    aplico rol premium

--- desafio-12-complementario/src/dao/managers/mongo/models/users.models.js
+++ desafio-12-complementario/src/dao/managers/mongo/models/users.models.js
@@ -30,7 +30,7 @@ const userSchema = new mongoose.Schema({
     },
     role: {
         type: String,
-        enum: ['admin', 'user'],
+        enum: ['admin', 'user', 'premium'],
         default: 'user'
     }
     
