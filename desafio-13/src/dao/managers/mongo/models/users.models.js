93e71470607c0ad64af45f1ac0cdb942508affc1
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-12-22 11:27:55 -0300

    copia desafio 12 para aplicar avances

---
+++ desafio-13/src/dao/managers/mongo/models/users.models.js
@@ -0,0 +1,50 @@
+import mongoose from 'mongoose';
+import { CartsManagerMongo } from '../cartsManagerMongo.js'; 
+import { logger } from '../../../../helpers/logger.js';
+
+const usersCollection = 'users';
+
+const userSchema = new mongoose.Schema({
+    // full_name: {
+    //     type: String,
+    //     //required: true,
+    // },
+    first_name: {
+        type: String,
+        required: true
+    },
+    last_name: String,
+    age: Number,
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    cart:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'carts'
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'user', 'premium'],
+        default: 'user'
+    }
+    
+})
+
+userSchema.pre('save', async function(next) {
+    try {
+        const cartsManager = new CartsManagerMongo()
+        const newCart = await cartsManager.createCart({})
+        this.cart = newCart._id
+    } catch (error) {
+        logger.error('error al crear el carrito con el usuario',error)
+        next(error)
+    }
+})
+
+export const usersModel = mongoose.model(usersCollection, userSchema);
---
+++ desafio-13/src/routes/usersSessions.routes.js
@@ -0,0 +1,34 @@
+import { Router } from "express";
+import { config } from "../config/config.js";
+import { 
+    registerLocalStrategy, registerGithubStrategy, 
+    registerGithubStrategyFail, loginLocalStrategy } from "../middlewares/auth.js";
+import { UsersSessionsController } from "../controller/usersSessions.controller.js";
+
+const router = Router();
+/*--------------- esctrategia registro local ---------------*/
+//registro al ususario
+router.post('/register', registerLocalStrategy, UsersSessionsController.renderRegister )
+
+router.get('/fail-register', UsersSessionsController.renderRegisterFail)
+/*----------------estrategia registro con github----------------*/
+//ruta registro con github
+router.get('/register-github', registerGithubStrategy)
+//ruta collback con github
+router.get(config.github.callbackUrl, registerGithubStrategyFail, UsersSessionsController.renderRegisterGithub)
+/*---------------- estrategia login ------------*/
+
+//logueo al usuario se admin o usuario locales//localhost:8080/api/sessions/login
+router.post('/login', loginLocalStrategy, UsersSessionsController.renderLogin)
+router.get('/fail-login', UsersSessionsController.renderLoginFail)
+
+/*--------------------------------------------------- */
+
+router.post('/forgot-password', UsersSessionsController.forgotPassword)
+router.post('/reset-password', UsersSessionsController.resetPassword)
+
+//para eliminar la seccion
+router.get('/logout', UsersSessionsController.renderLogout)
+
+
+export { router as usersSessionsRouter}
