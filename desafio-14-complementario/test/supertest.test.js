05154b41c6b09194ceb664eb0f62ce43f21ae57b
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2024-01-04 14:33:41 -0300

    aplico supertest

--- desafio-14-complementario/test/supertest.test.js
+++ desafio-14-complementario/test/supertest.test.js
@@ -1,47 +1,68 @@
-import dotenv from 'dotenv';
-dotenv.config()
-import {UsersManagerMongo } from '../src/dao/managers/mongo/usersManagerMongo.js'
-import mongoose from 'mongoose'
-import Assert  from 'assert'
-import { usersModel } from '../src/dao/managers/mongo/models/users.models.js'
+import { expect } from 'chai'
+import supertest from 'supertest'  
 
+const request = supertest('http://localhost:8080')
 
-const assert = Assert.strict
-//mongodb+srv://soledadar:g04D4zMd9O4y2GvK@cluster0.njbseut.mongodb.net/tiendaDB-Test?retryWrites=true&w=majority
-try {
-    await mongoose.connect(process.env.MONGO_URL_TEST)
-    console.log('Conectado a MongoDB Atlas Test')
-} catch (error) {
-   console.log(error.message) 
-}
+describe('Testing de ecommerce', () => {
 
-describe('Pruebas de modulos de usuarios dao', () => {
-    
-    beforeEach(async function(){
-        await usersModel.deleteMany({})
+    describe('Test de usuarios', () => {
+        it('El endpoint de Post /api/session/register debe registrar un usuario', async function () {
+            const newUser = {
+                first_name: 'test',
+                last_name: 'test',
+                email: 'test@test',
+                age: 10,
+                password: 'test'
+            }
+            const  registerResponse = await request.post('/api/session/register').send(newUser);
+            expect(registerResponse.status).to.be.eql(201);
+        })
     })
-
-    it('Prueba de creacion de usuarios', async () => {
-        const usersDao = new UsersManagerMongo()
-        const user = {
-            first_name: 'test',
-            last_name: 'test',
-            email: 'test@test',
-            age: 10,
-            password: 'test'
-        }
-        const result = await usersDao.createUsers(user)
-        console.log('resultado', result)
-        assert.strictEqual(result._id, true)
-
-    })
-
-    // it('Prueba que permite obtener todos los usuarios', async () => {
-    //     const usersdao = new UsersManagerMongo()
-    //     const result = await usersdao.getUsers()
-    //     console.log('resultado', result)
-
-    //     //comparacion valor actual y esperado
-    //     assert.strictEqual(Array.isArray(result), true)
-    // })
 })
+
+
+// import {UsersManagerMongo } from '../src/dao/managers/mongo/usersManagerMongo.js'
+// import mongoose from 'mongoose'
+// import Assert  from 'assert'
+// import { usersModel } from '../src/dao/managers/mongo/models/users.models.js'
+
+
+// const assert = Assert.strict
+
+// try {
+//     await mongoose.connect('')
+//     console.log('Conectado a MongoDB Atlas Test')
+// } catch (error) {
+//    console.log(error.message) 
+// }
+
+// describe('Pruebas de modulos de usuarios dao', () => {
+    
+//     beforeEach(async function(){
+//         await usersModel.deleteMany({})
+//     })
+
+//     it('Prueba de creacion de usuarios', async () => {
+//         const usersDao = new UsersManagerMongo()
+//         const user = {
+//             first_name: 'test',
+//             last_name: 'test',
+//             email: 'test@test',
+//             age: 10,
+//             password: 'test'
+//         }
+//         const result = await usersDao.createUsers(user)
+//         console.log('resultado', result)
+//         assert.strictEqual(result._id, true)
+
+//     })
+
+//     it('Prueba que permite obtener todos los usuarios', async () => {
+//         const usersdao = new UsersManagerMongo()
+//         const result = await usersdao.getUsers()
+//         console.log('resultado', result)
+
+//         //comparacion valor actual y esperado
+//         assert.strictEqual(Array.isArray(result), true)
+//     })
+// })
